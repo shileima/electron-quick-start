dcc8cd6db50a7c0529b641e7894ed207e5f82733
mashilei <mashilei@meituan.com>
2021-08-09 20:05:52 +0800

    feat: add react

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,27 +1,29 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
-    mount: {
-      /* ... */
-    },
-    plugins: [
-      /* ... */
-    ],
-    routes: [
-      /* Enable an SPA Fallback in development: */
-      // {"match": "routes", "src": ".*", "dest": "/index.html"},
-    ],
-    optimize: {
-      /* Example: Bundle your final build: */
-      // "bundle": true,
-    },
-    packageOptions: {
-      /* ... */
-    },
-    devOptions: {
-      /* ... */
-    },
-    buildOptions: {
-      /* ... */
-    },
-  };
-  
+  mount: {
+    // directory name: 'build directory'
+    public: '/',
+    src: '/dist',
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh'
+  ],
+
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
